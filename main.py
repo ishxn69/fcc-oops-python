4f758e0a4bde09489eadc0a558fde8434e727515
Ishan <gadekar.ishan@gmail.com>
2025-02-18 16:33:22 +1030

    magic methods, default values, self

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    #these methods with __ are known as magic methods.
+    #added a default value to quantity attribute
+    def __init__(self, name, price, quantity=0):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    
+    def calculate_total_price(self):
+        #no need to pass parameters externally now that we
+        #declared these attributes in constructor.
+        return self.price * self.quantity
 
-item1 = Item()
-item1.name = 'Phone'
-item1.price = 100
-item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
+item1 = Item('Phone', 100, 5)
 
-item2 = Item()
-item2.name = 'Laptop'
-item2.price = 1000
-item2.quantity = 3
-print(item2.calculate_total_price(item2.price, item2.quantity))
+item2 = Item('Laptop', 1000, 3)
+
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
+
