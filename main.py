4a3561f43fff3b383578b60bf9e2c059ba7632b3
Ishan <gadekar.ishan@gmail.com>
2025-02-20 08:17:56 +1030

    Added class list to store all instances so far

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 class Item:
-    pay_rate = 0.8 
+    pay_rate = 0.8
+    all = [] # Empty list to store all items of class. 
     
     def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
         
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
+        
+        # Actions to execute
+        Item.all.append(self) # This will store every instance inside the list. Keeps track of instances.
     
     def calculate_total_price(self):
         return self.price * self.quantity
     
-    #method to apply discount
     def apply_discount(self):
-        #if we use Item.pay_rate, we won't be able to change pay_rate for each instance
         self.price = self.price * self.pay_rate 
 
-item1 = Item('Phone', 100, 5)
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+print(Item.all)
 
-item2 = Item('Laptop', 1000, 3)
-
-item1.apply_discount()
-print(item1.price) #80.0
-item2.pay_rate = 0.7
-item2.apply_discount() #here we change pay_rate of class level to instance level
-print(item2.price) #700.0
-
-
+# Printing names of all the Instances of class so far.
+for instance in Item.all:
+    print(instance.name)
