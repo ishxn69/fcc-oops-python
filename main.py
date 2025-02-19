e3d3de32429ccad33c8ec4ef0c03e37098574731
Ishan <gadekar.ishan@gmail.com>
2025-02-20 07:51:23 +1030

    Added method to apply discount

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 class Item:
-    #added a class attribute
-    pay_rate = 0.8 #20% discount to all items
+    pay_rate = 0.8 
     
     def __init__(self, name: str, price: float, quantity=0):
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
         
-        #assigned values to self object
         self.name = name
         self.price = price
         self.quantity = quantity
     
     def calculate_total_price(self):
         return self.price * self.quantity
+    
+    #method to apply discount
+    def apply_discount(self):
+        #if we use Item.pay_rate, we won't be able to change pay_rate for each instance
+        self.price = self.price * self.pay_rate 
 
 item1 = Item('Phone', 100, 5)
 
 item2 = Item('Laptop', 1000, 3)
 
-print(Item.pay_rate)
-print(item1.pay_rate)
+item1.apply_discount()
+print(item1.price) #80.0
+item2.pay_rate = 0.7
+item2.apply_discount() #here we change pay_rate of class level to instance level
+print(item2.price) #700.0
 
-# __dict__ is an inbuilt magic attribute that returns a dictionary of 
-# all the attributes of a class for a class and all the attributes of
-# an instance of a class for an instance of a class.
-print(Item.__dict__)
-print(item1.__dict__)
 
