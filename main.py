549a6651c948b1b65b31a6a80f3eec7376b07c9a
Ishan <gadekar.ishan@gmail.com>
2025-03-26 05:49:33 +1030

    Change name var access to private, Add getter & setter for it

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # we import the 'Item' and 'Phone' classes from item.py 
 # and phone.py respectively
 from item import Item
-from phone import Phone
 
-phone1 = Phone('isgPhoneM12', 500, 6, 2)
+item1 = Item('Phone', 100)
 
-print(Item.all)
+# Below, we set name and print (Which is possible due
+# to getter and setter functions that we created.)
+# We do not need to specify __name, we can simply 
+# use the variable name without access modifier.
+
+# item1.name = 'AnotherItem' will return Exception as
+# It's length is above 10 chars
+item1.name = 'OtherItem' #this will work
+
+#validation check will run before value is printed.
+print(item1.name)
